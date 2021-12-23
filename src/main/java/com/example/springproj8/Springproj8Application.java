8aea33afdc13d89bf2b92b4c152d6df0a3c02675
ahmedvuqarsoy <ahmedvuqarsoy@gmail.com>
2021-12-23 13:41:52 +0400

    feature/lab12

--- src/main/java/com/example/springproj8/Springproj8Application.java
+++ src/main/java/com/example/springproj8/Springproj8Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springproj8Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springproj8Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Springproj8Application.class, args);
+    }
 
 }
